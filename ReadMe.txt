fc860525f0f71eb5f042e358930f52449995d2aa
Darren <Darrenjensen88@gmail.com>
2023-10-22 22:45:41 +0530

    started to code login pagae

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,18 @@
 1. change the wall paper with basic wallpaper.
-2. 
+_________________________
+
+Features.
+product name : TaskMinder (Agile task management software).
+menu 
+1. login.( U -id , password , roll)
+2. product backlog.(USer story number, description)
+3. sprint tasks.
+4. logout.
+5. home.
+__________________________
+
+1. creating a portal, product owner can create the product backlogs(user story)
+2. scrum master asigns the user story to the scrum team.
+3. scrum team can pick user story, work on it and change it accordingly.
+
+
--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-import  express  from "express";
+import express from "express";
+import bodyParser from "body-parser";
+import { name } from "ejs";
+
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/" , (req,res)=> {
+app.get("/", (req, res) => {
 	res.render("index.ejs");
 })
-app.get("/To-Do",(req,res)=>{
+
+app.get("/To-Do", (req, res) => {
 	res.render("ToDo.ejs");
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
 	res.render("about.ejs");
 })
 
-app.post("/submit", (req,res)=>{
-	res.render("ToDo.ejs");
+app.get("/login", (req, res) => {
+	res.render("log.ejs")
 })
-app.listen(3000 , ()=>{
+
+app.post("/login", (req,res)=>{
+	//console.log(req.body.User);
+	const valid = req.body.User;
+	 
+	if(valid === "Darren" ){
+		res.render("ToDo.ejs");
+	}else{
+		console.log("wrong user");
+	} 
+})
+
+app.get("/signUp", (req, res) => {
+	res.render("sign.ejs");
+})
+
+
+
+app.listen(3000, () => {
 	console.log("the server is running on port 3000");
 })
