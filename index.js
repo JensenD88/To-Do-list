84bec4cab5adae8443a308ab586be3c0e45d25ff
Darren <Darrenjensen88@gmail.com>
2023-10-23 21:40:43 +0530

    signUp and basic authentication was built for login with Database

--- index.js
+++ index.js
@@ -1,43 +1,93 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
+//connecting to mondodb named UserList using mongose ODM
+mongoose.connect("mongodb://localhost:27017/UserList", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const userList = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	Password: {
+		type: String, required: true
+	},
+	role: String
+})
+//creating a model
+const User = new mongoose.model("users", userList);
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index.ejs");
 })
 
-app.get("/To-Do", (req, res) => {
-	res.render("ToDo.ejs");
-})
+
 
 app.get("/about", (req, res) => {
 	res.render("about.ejs");
+})
+
+
+app.get("/signUp", (req, res) => {
+	res.render("sign.ejs");
+})
+
+app.post("/signUp", (req, res) => {
+
+	const mailId = req.body.Email;
+	const passw = req.body.Password;
+	const Rl = req.body.role;
+
+	var newUser = new User({
+		email: mailId,
+		Password: passw,
+		role: Rl
+	})
+
+	newUser
+		.save()
+		.then(() => {
+			console.log(newUser);
+			console.log("saved User successfully")
+			res.render("ToDo.ejs");
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+  User.close;
 })
 
 app.get("/login", (req, res) => {
 	res.render("log.ejs")
 })
 
-app.post("/login", (req,res)=>{
-	//console.log(req.body.User);
-	const valid = req.body.User;
-	 
-	if(valid === "Darren" ){
-		res.render("ToDo.ejs");
-	}else{
-		console.log("wrong user");
-	} 
-})
+app.post("/login", (req, res) => {
+	var mailEntered = req.body.mail;
+	var passwordEntered = req.body.password;
 
-app.get("/signUp", (req, res) => {
-	res.render("sign.ejs");
-})
+	User.findOne({ email: mailEntered })
+		.then((data) => {
 
+			if (data) {
+				//checking password 
+				console.log("user exists")
+				if (data.Password === passwordEntered) {
+					res.render("ToDo.ejs");
+					console.log("login successfull")
+				}else{res.send("incorrect password")}
+			}else{res.send("USER not found")}
+		}
+		)
+})
 
 
 app.listen(3000, () => {
